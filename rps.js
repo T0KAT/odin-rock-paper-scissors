fbef86c13b04d8568d2517790eb96b907e0e94c8
2CAT <156127037+T0KAT@users.noreply.github.com>
2024-02-29 21:33:20 +0100

    changed playRound to incorparate caseinsensivty

--- rps.js
+++ rps.js
@@ -1,62 +1,41 @@
-
-
-
-//:create a function that gives the randomized outcome of either "rock,paper or scissor"
-
-
-const computerChoice = ["Rock", "Paper", "Scissor"];
+const computerChoices = ["Rock", "Paper", "Scissor"];
 
 function getComputerChoice() {
-    let randomIndex = Math.floor(Math.random() * computerChoice.length),
-        randomElement = computerChoice[randomIndex];
-    return randomElement;    
+    let randomIndex = Math.floor(Math.random() * computerChoices.length);
+    return computerChoices[randomIndex]; // This will return 'Rock', 'Paper', or 'Scissor'
 };
 
+function playRound(playerSelection, computerSelection) {
+    // Convert both selections to lowercase
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
 
-
-//: create a function that lets me Enter Rock paper or scissors
-
-
-function playRound (playerSelection, computerSelection) {
-
-    
-    
-    if (playerSelection == "Rock") {
-        if (computerSelection == "Paper") {
-            return console.log("You chose rock and the computer chose paper. You Loose!!");
+    if (playerChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You chose rock and the computer chose paper. You lose!!");
+        } else if (computerChoice === "rock") {
+            console.log("You chose rock and the computer chose rock. It's a draw!!!");
+        } else {
+            console.log("You chose rock and the computer chose scissor. You win!!");
         }
-        else if (computerSelection == "Rock") {
-            return console.log("You chose rock and the computer chose rock. It's a draw!!!");
+    } else if (playerChoice === "paper") {
+        if (computerChoice === "scissor") {
+            console.log("You chose paper and the computer chose scissor. You lose!!");
+        } else if (computerChoice === "paper") {
+            console.log("You chose paper and the computer chose paper. It's a draw!!!");
+        } else {
+            console.log("You chose paper and the computer chose rock. You win!!");
         }
-        else 
-            return console.log("You chose rock and the computer chose scissor. You win!!");
+    } else if (playerChoice === "scissor") {
+        if (computerChoice === "rock") {
+            console.log("You chose scissor and the computer chose rock. You lose!!");
+        } else if (computerChoice === "scissor") {
+            console.log("You chose scissor and the computer chose scissor. It's a draw!!!");
+        } else {
+            console.log("You chose scissor and the computer chose paper. You win!!");
+        }
     }
-
-    else if (playerSelection == "Paper") {
-        if (computerSelection == "Scissor") {
-            return console.log("You chose paper and the computer chose scissor. You Loose!!");
-        }
-        else if (computerSelection == "Paper") {
-            return console.log("You chose paper and the computer chose paper. It's a draw!!!");
-        }
-        else 
-            return console.log("You chose paper and the computer chose rock. You win!!");
-    }
-
-    else if (playerSelection == "Scissor") {
-        if (computerSelection == "Rock") {
-            return console.log("You chose scissor and the computer chose rock. You Loose!!");
-        }
-        else if (computerSelection == "Scissor") {
-            return console.log("You chose scissor and the computer chose scissor. It's a draw!!!");
-        }
-        else 
-            return console.log("You chose scissor and the computer chose paper. You win!!");
-    }
-        
-    
-
 }
 
-
-playRound("Rock", getComputerChoice())
+// Now you can enter "rock", "Rock", "ROCK", etc.
+playRound("SCIssor", getComputerChoice());
