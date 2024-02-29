ed0fc580156b85f09f42fd0c84c03d20c97d1abb
2CAT <156127037+T0KAT@users.noreply.github.com>
2024-02-29 21:11:58 +0100

    Created a playRound function with the basic concept of rock-paper-scissor

--- rps.js
+++ rps.js
@@ -12,4 +12,51 @@ function getComputerChoice() {
     return randomElement;    
 };
 
-console.log(getComputerChoice())
+
+
+//: create a function that lets me Enter Rock paper or scissors
+
+
+function playRound (playerSelection, computerSelection) {
+
+    
+    
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Paper") {
+            return console.log("You chose rock and the computer chose paper. You Loose!!");
+        }
+        else if (computerSelection == "Rock") {
+            return console.log("You chose rock and the computer chose rock. It's a draw!!!");
+        }
+        else 
+            return console.log("You chose rock and the computer chose scissor. You win!!");
+    }
+
+    else if (playerSelection == "Paper") {
+        if (computerSelection == "Scissor") {
+            return console.log("You chose paper and the computer chose scissor. You Loose!!");
+        }
+        else if (computerSelection == "Paper") {
+            return console.log("You chose paper and the computer chose paper. It's a draw!!!");
+        }
+        else 
+            return console.log("You chose paper and the computer chose rock. You win!!");
+    }
+
+    else if (playerSelection == "Scissor") {
+        if (computerSelection == "Rock") {
+            return console.log("You chose scissor and the computer chose rock. You Loose!!");
+        }
+        else if (computerSelection == "Scissor") {
+            return console.log("You chose scissor and the computer chose scissor. It's a draw!!!");
+        }
+        else 
+            return console.log("You chose scissor and the computer chose paper. You win!!");
+    }
+        
+    
+
+}
+
+
+playRound("Rock", getComputerChoice())
